8781ae5e038f6e2fcc761d326576031e0115f6c0
lponisio <lponisio@gmail.com>
2019-04-01 17:13:00 -0700

    tried out analyses where I drop interactions not found in 2014 and compare robustness between years. Not terribly interesting

--- analysis/networkLevel/robustness.R
+++ analysis/networkLevel/robustness.R
@@ -3,9 +3,8 @@ rm(list=ls())
 setwd('analysis/networkLevel')
 source('src/initialize.R')
 
-extinction.methods <- c("veg", "degree", "visit")
+extinction.methods <- c("visit") ## visit, degree, veg
 participants<- c("lower")
-by.abund <- c("abund")
 
 ## **********************************************************
 ## robustness
@@ -13,59 +12,36 @@ by.abund <- c("abund")
 ## simulate plant extinction
 ## Simpson div pyrodiversity
 
-split.pol <- split(spec, spec$GenusSpecies)
-all.plant <- lapply(split.pol, function(x) unique(x$PlantGenusSpecies))
-
-filled.nets <- lapply(nets, function(y){
-    y[y > 1] <- 1
-    for(sp in colnames(y)){
-        this.int <- y[,sp]
-        this.ref <- all.plant[[sp]]
-        in.ref <- names(this.int) %in% this.ref
-        y[,sp][in.ref] <- 1
-    }
-    return(y)
-})
-
-
-
-all.nets <- list(nets, filled.nets)
-names(all.nets) <- c("obs", "potential")
-
 for(net.type in names(all.nets)){
     for(sp.level in participants){
         for(ex.method in extinction.methods){
-            for(ab in by.abund){
-                ext.rows <- getExtinctionOrder(ex.method,
-                                               by.abund=ab,
-                                               nets,
-                                               spec,
-                                               veg)
-                res <- simExtinction(all.nets[[net.type]],
-                                     extinction.method="external",
-                                     dat.mods,
-                                     participant=sp.level,
-                                     ext.row=ext.rows)
-                res$Year <- factor(res$Year,
-                                   levels=c("2013", "2014"))
+            ext.rows <- getExtinctionOrder(ex.method,
+                                           all.nets[[net.type]],
+                                           spec,
+                                           veg)
+            res <- simExtinction(all.nets[[net.type]],
+                                 extinction.method="external",
+                                 dat.mods,
+                                 participant=sp.level,
+                                 ext.row=ext.rows)
+            res$Year <- factor(res$Year,
+                               levels=c("2013", "2014"))
 
-                mod.div <- lmer(Robustness ~
-                                    scale(simpson.div)*Year
-                                + (1|Site),
-                                data=res)
+            mod.div <- lmer(Robustness ~
+                                scale(simpson.div)*Year
+                            + (1|Site),
+                            data=res)
 
-                print(paste("*******", net.type, ex.method, sp.level, ab, "*******"))
-                print(summary(mod.div))
-                ## print(anova(mod.div))
-                save(mod.div, res,
-                     file=file.path(save.path,
-                                    sprintf('mods/robustness_%s_%s_%s_%s.Rdata',
-                                            ex.method, sp.level, net.type, ab)))
-            }
+            print(paste("*******", net.type, ex.method, sp.level, "*******"))
+            print(summary(mod.div))
+            ## print(anova(mod.div))
+            save(mod.div, res,
+                 file=file.path(save.path,
+                                sprintf('mods/robustness_%s_%s_%s.Rdata',
+                                        ex.method, sp.level, net.type)))
         }
     }
 }
-
 
 
 diff.mats  <-  mapply(function(a, b){b-a},
